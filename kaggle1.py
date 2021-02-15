eda31d2c19ea02368596c235a6ad8ec9d7f23767
study <sswwd95@gmail.com>
2021-02-15 18:01:35 +0900

    project

---
+++ kaggle1.py
@@ -0,0 +1,4 @@
+import numpy as np
+import pandas as pd
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D, BatchNormalization, Flatten
+from tensorflow.keras.models import Sequential, model_from_json
