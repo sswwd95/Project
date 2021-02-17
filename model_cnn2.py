48e5cb4f368a36727629f5af8399c369d72bd4d6
study <sswwd95@gmail.com>
2021-02-17 11:55:11 +0900

    project

--- model_cnn2.py
+++ model_cnn2.py
@@ -1,15 +1,55 @@
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
-import os
-import cv2
-import pandas as pd
-from keras.optimizers import Adam
+import cv2, os
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, Dropout, BatchNormalization
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.utils import to_categorical
 
-X = np.load('../project/npy/X_train.npy')
-Y = np.load('../project/npy/Y_train.npy')
-X_eval = np.load('../project/npy/X_eval.npy')
-Y_eval = np.load('../project/npy/Y_eval.npy')
+train_dir = "../project/asl-alphabet/asl_alphabet_train/asl_alphabet_train"
+eval_dir =  "../project/asl-alphabet-test"
+
+def load_images(directory):
+    X = [] #image
+    Y = [] #label
+    for idx, label in enumerate(uniq_labels):
+        for file in os.listdir(directory +'/' + label):
+            filepath = directory + '/' + label + '/' + file
+            image = cv2.resize(cv2.imread(filepath), (64,64))
+            # 64x64크기의 이미지
+            X.append(image)
+            Y.append(idx)
+    X = np.array(X).astype('float32')/255.
+    Y = np.array(Y).astype('float32')
+    Y = to_categorical(Y, num_classes=29)
+
+    return(X,Y)
+'''
+print(X, Y)
+X =[[[[230   2   4]
+    [187   9   9]
+    [183  10  14]
+    ...
+    [190  17  23]
+    [188  17  20]
+    [212  12  15]]
+
+Y = [ 0  0  0 ... 28 28 28]
+'''
+   
+uniq_labels = sorted(os.listdir(train_dir)) # sorted() -> 정렬함수
+X,Y = load_images(directory=train_dir)
+
+print(X.shape, Y.shape) #(87000, 64, 64, 3) (87000,)
+
+if uniq_labels == sorted(os.listdir(eval_dir)):
+    X_eval, Y_eval = load_images(directory=eval_dir)
+
+# 트레인 폴더의 파일 리스트와 검증폴더의 파일 리스트가 같다면 트레인 폴더와 같게 x, y를 나눈다.
+
+print(X_eval.shape, Y_eval.shape) #(870, 64, 64, 3) (870,)
+
 
 print(X.shape, Y.shape) #(87000, 64, 64, 3) (87000,)
 print(" Max value of X: ",X.max())
@@ -53,6 +93,10 @@ from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(
     X, Y, test_size=0.2, random_state=42, stratify=Y)
 
+x_train, x_val, y_train, y_val = train_test_split(
+    x_train, y_train, test_size=0.1, random_state=42
+)
+
 # x_train = x_train.reshape(-1,64,64,3)
 
 print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
@@ -62,44 +106,30 @@ from keras.layers import Conv2D, Dense, Dropout, Flatten,MaxPooling2D,BatchNorma
 from keras.models import Sequential
 
 model = Sequential()
+model.add(Conv2D(64, (3,3), input_shape=(64,64,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(filters=256,kernel_size=(5,5),activation="relu",padding="same",input_shape=(64,64,3)))
-model.add(Conv2D(filters=256,kernel_size=(5,5),activation="relu",padding="same"))
-model.add(MaxPooling2D(pool_size=(2,2),padding="same"))
+model.add(Conv2D(64, (3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(filters=128,kernel_size=(4,4),activation="relu",padding="same"))
-model.add(Conv2D(filters=128,kernel_size=(4,4),activation="relu",padding="same"))
-model.add(MaxPooling2D(pool_size=(2,2),padding="same"))
-
-model.add(Dropout(0.2))
-
-model.add(Conv2D(filters=64,kernel_size=(3,3),activation="relu",padding="same"))
-model.add(Conv2D(filters=64,kernel_size=(3,3),activation="relu",padding="same"))
-model.add(MaxPooling2D(pool_size=(2,2),padding="same"))
-
-model.add(Dropout(0.2))
-
-model.add(Conv2D(filters=32,kernel_size=(2,2),activation="relu",padding="same"))
-model.add(MaxPooling2D(pool_size=(2,2),padding="same"))
-
-model.add(Dropout(0.2))
+model.add(Conv2D(64, (3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())
-
-model.add(Dense(128,activation="relu"))
-model.add(Dense(64,activation="relu"))
-model.add(Dense(40,activation="relu"))
-
-model.add(Dense(29,activation="softmax"))
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.20))
+model.add(Dense(29, activation='softmax'))
 model.summary()
 
 from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping, ReduceLROnPlateau,ModelCheckpoint
 es=EarlyStopping(patience=20, verbose=1, monitor='val_loss',restore_best_weights = True)
 rl=ReduceLROnPlateau(patience=10, verbose=1, monitor='val_loss')
+filepath = '..project/modelcp/cnn2.h5'
+cp = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
 
-model.compile(optimizer=Adam(learning_rate=0.01), loss = 'categorical_crossentropy', metrics = ['accuracy'])
-hist=model.fit(x_train, y_train, epochs = 5,callbacks=[es,rl], batch_size = 64, validation_data=(x_test,y_test))
+model.compile(optimizer=Adam(learning_rate=0.001), loss = 'categorical_crossentropy', metrics = ['accuracy'])
+hist=model.fit(x_train, y_train, epochs = 50,callbacks=[es,rl,cp], batch_size = 128, validation_data=(x_val,y_val))
 
 model.save('../project/h5/cnn2.h5')
 
